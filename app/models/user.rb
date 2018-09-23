25908ca050cd1dea632bc8dd1859ee7eafcf541f
Sany18 <parubok.sashko@gmail.com>
2018-09-23 14:19:40 +0300

    cvm

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
          :recoverable,
          :rememberable,
          :validatable,
-         :confirmable
+         # :confirmable
 
   validates :first_name, presence: true
 end
