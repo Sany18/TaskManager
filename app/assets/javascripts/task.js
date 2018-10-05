c9b01865b8c3f26b737bf7d3b98ccf51dbec97e5
Sany18 <parubok.sashko@gmail.com>
2018-10-05 16:21:24 +0300

    Swiched to jQuery

--- app/assets/javascripts/task.js
+++ app/assets/javascripts/task.js
@@ -1,121 +1,147 @@
-var HEROKUROOT = "";
+$(document).ready(function () {
+  var HEROKUROOT = "";
 
-function checkboxes_set_all(status) {
-  var items = document.getElementsByClassName(status);
-  for (let i = 0; i < items.length; i++) {
-    if (items[i].checked != true) {
-      items[i].checked = true;
+  $(".checkboxes_set_all_0").click(() => {
+    change_checkboxes(".checkbox_belongs_0")
+  });
+  $(".checkboxes_set_all_1").click(() => {
+    change_checkboxes(".checkbox_belongs_1")
+  });
+
+  function change_checkboxes(checkboxes_class) {
+    var items = $(checkboxes_class);
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].checked != true) {
+        items[i].checked = true;
+      } else {
+        items[i].checked = false;
+      }
+    }
+  }
+
+  $(".drop_down").click((x = $(this)) => {
+    curent_list = x.currentTarget.className.replace("w3-dropdown-click drop_down ", "");
+    var button = $("#" + curent_list);
+    if (button.hasClass("w3-show") === false) {
+      button.addClass("w3-show");
     } else {
-      items[i].checked = false;
+      button.removeClass("w3-show");
+    }
+  });
+
+  $(".close_new_task").click(() => {
+    $("#new_task").css("display", "none").html("");
+  });
+
+  $(".new_task").click(() => {
+    var notice = $('#notice');
+    var new_task_div = $("#new_task");
+
+    console.log(new_task_div.css("display"));
+    if (new_task_div.css("display") === "none") {
+      notice.html("Wait");
+      var w = setInterval(() => {
+        notice.html(notice.text() + ".")
+      }, 250);
+
+      $.get(HEROKUROOT + '/tasks/new')
+        .done((data) => {
+          new_task_div.html(data).css("display", "block");
+        })
+        .fail(() => {
+          clearInterval(w);
+          notice.html("We have some problem");
+        })
+        .always(() => {
+          clearInterval(w);
+          notice.html("");
+        });
+    } else {
+      new_task_div.css("display", "none").html("");
+      notice.html("");
+    }
+  });
+
+  $(".slider").on("input change", () => {
+    let slider = $(".slider");
+    let value = slider.val();
+    let output = $("#show_value");
+    output.html(value);
+    slider.css("background-color", getColor(value));
+    console.log("Hello, mr. " + getColor(value));
+  });
+
+  function getColor(id) {
+    switch (+id) {
+      case 1:
+        return "Lightgreen";
+      case 2:
+        return "Green";
+      case 3:
+        return "Yellow";
+      case 4:
+        return "Orange";
+      case 5:
+        return "Orangered";
+      default:
+        return "Green";
     }
   }
-}
 
-function dropDown(id) {
-  var x = document.getElementById("dropList" + id);
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else {
-    x.className = x.className.replace(" w3-show", "");
-  }
-}
+  $(".destroy_selected_0").click(() => {
+    destroy_selected(".checkbox_belongs_0")
+  });
+  $(".destroy_selected_1").click(() => {
+    destroy_selected(".checkbox_belongs_1")
+  });
 
-function newTask() {
-  var notice = document.getElementById('notice');
-  notice.innerHTML = "Wait";
-  var w = setInterval(() => {
-    notice.innerHTML = notice.innerHTML + ".";
-  }, 250);
-  var z = document.getElementById("newTask");
-  if (z.innerHTML == "") {
-    console.log('im work');
-    var myInit = {
-      method: 'GET',
-      credentials: 'include'
-    };
-    fetch(HEROKUROOT + '/tasks/new', myInit)
-      .then(function (address) {
-        return address.text();
-      })
-      .then(function (body) {
-        z.innerHTML = body;
-        clearInterval(w);
-        notice.innerHTML = "";
-      });
-  } else {
-    z.innerHTML = "";
-    clearInterval(w);
-    notice.innerHTML = "";
-  }
-}
 
-function showValue(value) {
-  var output = document.getElementById("showValue");
-  output.innerHTML = value;
-  output.style.backgroundColor = getColor(value);
-  console.log("Hello, mr. " + getColor(value));
-}
-
-function getColor(id) {
-  switch (+id) {
-    case 1:
-      return "Lightgreen";
-    case 2:
-      return "Green";
-    case 3:
-      return "Yellow";
-    case 4:
-      return "Orange";
-    case 5:
-      return "Orangered";
-    default:
-      return "Green";
-  }
-}
-
-function destroySelected(status) {
-  var path = HEROKUROOT + "/task/delete_selected/";
-  var myInit = {
-    method: 'DELETE',
-    credentials: 'include'
-  };
-  var items = document.getElementsByClassName(status);
-  for (let i = 0; i < items.length; i++) {
-    if (items[i].checked == true) {
-      path += items[i].id + "%";
+  function destroy_selected(checkboxes_class) {
+    var path = HEROKUROOT + "/task/delete_selected/";
+    var items = $(checkboxes_class);
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].checked == true) {
+        path += items[i].id + "%";
+      }
     }
+    var notice = $('#notice');
+    notice.innerHTML = "Wait";
+    var w = setInterval(() => {
+      notice.innerHTML = notice.innerHTML + ".";
+    }, 250);
+
+    $.ajax({
+      url: path,
+      type: 'DELETE',
+      success: function (result) {
+        window.location = "/";
+      }
+    })
   }
-  var notice = document.getElementById('notice');
-  notice.innerHTML = "Wait";
-  var w = setInterval(() => {
-    notice.innerHTML = notice.innerHTML + ".";
-  }, 250);
-  fetch(path, myInit)
-    .then(() => window.location = "/");
-}
 
-window.onload = function () {
-  var buttonMove = document.getElementById("kittien");
-  buttonMove.onmousedown = function (a) {
-    function b(a) {
-      buttonMove.style.top = a.pageY - c + "px";
-      buttonMove.style.left = a.pageX - d + "px";
-    }
+  (() => {
+    var buttonMove = document.getElementById("kittien");
+    buttonMove.onmousedown = function (a) {
+      function b(a) {
+        buttonMove.style.top = a.pageY - c + "px";
+        buttonMove.style.left = a.pageX - d + "px";
+      }
 
-    var c = a.pageY - buttonMove.getBoundingClientRect().top + pageYOffset,
-      d = a.pageX - buttonMove.getBoundingClientRect().left + pageXOffset;
-    buttonMove.style.position = "absolute";
-    buttonMove.style.position = "fixed";
-    b(a);
-    document.onmousemove = function (a) {
+      var c = a.pageY - buttonMove.getBoundingClientRect().top + pageYOffset,
+        d = a.pageX - buttonMove.getBoundingClientRect().left + pageXOffset;
+      buttonMove.style.position = "absolute";
+      buttonMove.style.position = "fixed";
       b(a);
+      document.onmousemove = function (a) {
+        b(a);
+      };
+      window.onmouseup = function () {
+        document.onmousemove = null;
+        buttonMove.onmouseup = null;
+      }
     };
-    window.onmouseup = function () {
-      document.onmousemove = null;
-      buttonMove.onmouseup = null;
-    }
-  };
-  buttonMove.ondragstart = function () {
-    return !1;
-  };
-};
+    buttonMove.ondragstart = function () {
+      return !1;
+    };
+  })();
+});
