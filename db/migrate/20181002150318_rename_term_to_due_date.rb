1d00744de5b3c91cdd4eccd987ce0d20261726c9
Sany18 <parubok.sashko@gmail.com>
2018-10-09 11:22:27 +0300

    Commit

--- db/migrate/20181002150318_rename_term_to_due_date.rb
+++ db/migrate/20181002150318_rename_term_to_due_date.rb
@@ -1,11 +1,11 @@
 class RenameTermToDueDate < ActiveRecord::Migration[5.2]
   def change
-    def self.up
-      rename_column :task, :term, :due_date
-    end
-
-    def self.down
-      rename_column :task, :due_date, :term
-    end
+    # def self.up
+    #   rename_column :tasks, :term, :due_date
+    # end
+    #
+    # def self.down
+    #   rename_column :tasks, :due_date, :term
+    # end
   end
 end
